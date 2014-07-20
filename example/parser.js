e8af58cbe9fb27c4be93e562006d74671e175309
Jason Brown <jbrown@jamasoftware.com>
2014-07-19 21:26:20 -0700

    Initial commit

---
+++ example/parser.js
@@ -0,0 +1,10 @@
+var React = require('react'),
+	reactParser = require('./../lib/index');
+
+
+var html = '<div data-id="1" class="hey this is a class" style="width:100%;height: 100%;"><article id="this-article"><p>hey this is a paragraph</p><div><ul><li>1</li><li>2</li><li>3</li></ul></div></article></div>';
+
+var parsedComponent = reactParser(html);
+
+console.log('Original : ' + html);
+console.log('Converted: ' + React.renderComponentToStaticMarkup(parsedComponent));
